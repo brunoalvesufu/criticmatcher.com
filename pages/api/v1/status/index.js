8ea877b84557747a5421ab5c601c82b50ec2ad18
brunoalvesufu <brunoalves.ufu@gmail.com>
2025-05-21 00:05:29 -0300

    added  and

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-function status(resquest, response) {
-  response.status(200).json({ mensagem: "Tudo funcionando! Çàê" })
+import database from "../../../../infra/database.js";
+
+async function status(resquest, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;")
+  console.log("The result:")
+  console.log(result.rows);
+  response.status(200).json({ mensagem: "Tudo funcionando! Çàê" });
 }
 
 export default status;
